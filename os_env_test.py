21198ff3cd6e2508d9beaf0b8a9b7120661784a2
Surya10d <104970523+Surya10d@users.noreply.github.com>
2024-01-05 12:53:34 +0530

    Update os_env_test.py

--- os_env_test.py
+++ os_env_test.py
@@ -5,3 +5,8 @@ os.environ['MY_VARIABLE'] = 'Hello, GitHub Actions!'
 
 # Print the value
 print(f"MY_VARIABLE set to: {os.environ['MY_VARIABLE']}")
+
+env_file = os.getenv('GITHUB_ENV')
+if env_file:
+    with open(env_file, "a") as myfile:
+        myfile.write("MY_VARIABLE=1\n")
